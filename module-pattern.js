d2d7e266f070b8b536d6a5b39036a4a1afab3283
neoandrevictor <neoandrevictor@hotmail.com>
2019-02-16 14:56:51 -0200

    Add files via upload

--- module-pattern.js
+++ module-pattern.js
@@ -1,10 +1,31 @@
 console.log("===Modulle Pattern===");
 
 //IIFE
-(function (){
+const MODULE=(function (){
     var APP="Demo";
-    console.log(APP);
+
+    var box={};
     
+    box.count=0;
+
+
+   box.addCount=function(){
+       box.count++;
+       console.log(box.count);
+       return box.count;
+   }
+
+   box.resetCount=function(){
+        box.count=0;
+        return box.count;
+    }
+
+
+   //
+    return {add:box.addCount,reset:box.resetCount};
+
+    
+    console.log(APP,box);
 })();
 
 
